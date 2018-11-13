2f12d63dbd28bd39a72c71a0e220dc67978e4ac4
Sharath Golluri <sharathgolluri@gmail.com>
2018-11-13 05:58:43 +0530

    Signup window
    Final edits on the event handling side.

--- src/MyPortal/SignUpController.java
+++ src/MyPortal/SignUpController.java
@@ -15,9 +15,70 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class SignUpController implements Initializable {
+public class SignUpController implements Initializable
+{
+    public TextField nameTextField;
+    public TextField dateofbirthTextField;
+    public TextField emailTextField;
+    public TextField addressTextField;
+    public TextField usernameTextField;
+    public PasswordField passwordPasswordField;
+    public Button signupButton;
+    public Button loginButton;
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        System.out.println("Register kar BC!");
+        signupButton.setOnAction(new EventHandler<ActionEvent>()
+            {
+                @Override
+                public void handle(ActionEvent actionEvent)
+                {
+                    Stage currentWindow = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+                    // boolean authenticated = false;
+                    JDBCDriverConnection.connect();
+                    //JDBCDriverConnection.CreateUser(nameTextField.getText(), dateofbirthTextField.getText() , emailTextField.getText() , addressTextField.getText() , usernameTextField.getText(),passwordPasswordField.getText());
+                    JDBCDriverConnection.disconnect();
+                    if(true)
+                    {
+                        Parent myPortal = null;
+                        try{
+                            myPortal = FXMLLoader.load(getClass().getResource("MyPortal.fxml"));
+                        }
+                        catch (Exception e){
+                            System.out.println(e);
+                        }
+                        currentWindow.setTitle("Register");
+                        currentWindow.setScene(new Scene(myPortal  , 1280  , 720));
+                        currentWindow.show();
+                    }
+                   // System.out.println();
+
+                }
+            }
+        );
+        loginButton.setOnAction(new EventHandler<ActionEvent>()
+        {
+            @Override
+            public void handle(ActionEvent actionEvent)
+            {
+                Stage currentWindow = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+                Parent login = null;
+                try
+                    {
+                        login = FXMLLoader.load(getClass().getResource("MyPortal.fxml"));
+                    }
+                catch (Exception e)
+                    {
+                        System.out.println(e);
+                    }
+
+                currentWindow.setTitle("Back to login window");
+                currentWindow.setScene(new Scene(login  , 1280  , 720));
+                currentWindow.show();
+            }
+        });
 
     }
 
