3cbe110b4a3db701607e8806424124e321b86761
Sharath Golluri <sharathgolluri@gmail.com>
2018-11-13 05:47:40 +0530

    Signup window

---
+++ src/MyPortal/SignUpController.java
@@ -0,0 +1,25 @@
+package MyPortal;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+}
+
